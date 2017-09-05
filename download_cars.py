2ff26192e82b62cb023a09f049d8769540196b2e
Andis Cirulis <andis.cirulis@whitedigital.eu>
2017-09-05 21:13:28 +0300

    yolo annotation format fix

--- download_cars.py
+++ download_cars.py
@@ -1,15 +1,25 @@
 from pycocotools.coco import COCO
-import numpy as np
-import matplotlib.pyplot as plt
-import pylab
 import urllib.request
 import os
-
-pylab.rcParams['figure.figsize'] = (10.0, 8.0)
 
 dataDir = '.'
 dataType = 'train2017'
 annFile = '%s/annotations/instances_%s.json' % (dataDir, dataType)
+
+
+def convert(size, box):
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[1]) / 2.0
+    y = (box[2] + box[3]) / 2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return (x, y, w, h)
+
 
 coco = COCO(annFile)
 
@@ -18,9 +28,12 @@ cats = coco.loadCats(coco.getCatIds())
 nms = [cat['name'] for cat in cats]
 print('COCO categories: \n\n', '; '.join(nms))
 
-
-# get all images containing ALL given categories, select one at random
+# get all images containing ALL given categories
 catIds = coco.getCatIds(catNms=['truck'])
+catIds_all = coco.getCatIds(catNms=['car', 'truck', 'bus', 'train'])
+catNames = {0: 'car', 1: 'truck', 2: 'bus', 3: 'train'}
+catMap = {3: 0, 6: 1, 7: 2, 8: 3}
+
 imgIds = coco.getImgIds(catIds=catIds)
 print('Total images found: ', len(imgIds))
 
@@ -35,11 +48,16 @@ for img in imgs:
         print('%s already exists' % img['file_name'])
     else:
         urllib.request.urlretrieve('http://mscoco.org/images/%d' % (img['id']), img_fname)
-        annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+        annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds_all, iscrowd=None)
         anns = coco.loadAnns(annIds)
 
         with open(ann_fname, "w", newline='\n') as text_file:
             for ann in anns:
-                text_file.write('0 %s\n' % ' '.join([str(x) for x in ann['bbox']]))
+                x = ann['bbox'][0]
+                y = ann['bbox'][1]
+                w = ann['bbox'][2]
+                h = ann['bbox'][3]
+                cx, cy, cw, ch = convert((img['width'], img['height']), (x, x + w, y, y + h))
+                text_file.write('%s %s\n' % (catMap[ann['category_id']], ' '.join([str(x) for x in [cx, cy, cw, ch]])))
         print('%s downloaded (%s of %s) ' % (img['file_name'], cnt, len(imgIds)))
     cnt = cnt + 1
